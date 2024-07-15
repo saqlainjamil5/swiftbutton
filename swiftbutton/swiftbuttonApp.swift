ba4ae1b9f10b9deec615d7f5519163479163452b
Saqlainjamil5 <80804975+saqlainjamil5@users.noreply.github.com>
2024-07-15 14:48:21 +0500

    Initial Commit

---
+++ swiftbutton/swiftbuttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftbuttonApp.swift
+//  swiftbutton
+//
+//  Created by Saqlain Jamil on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftbuttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
